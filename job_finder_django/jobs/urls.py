fc7eb8d1fcb4abb4f725c54722ca91d1348a1fe3
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-20 17:57:16 +0200

    basic_url

--- job_finder_django/jobs/urls.py
+++ job_finder_django/jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('find_job/', views.find_job, name='find_job'),
     path('display_stats/', views.display_stats, name='display_stats'),
     path('personalized_offers/', views.personalized_offers, name='personalized_offers'),
