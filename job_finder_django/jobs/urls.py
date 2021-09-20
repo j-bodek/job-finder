d033495a735982afc698d281173a9095bd55c1e8
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-20 19:06:06 +0200

    sticky_footer

--- job_finder_django/jobs/urls.py
+++ job_finder_django/jobs/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('find_job/', views.find_job, name='find_job'),
     path('display_stats/', views.display_stats, name='display_stats'),
     path('personalized_offers/', views.personalized_offers, name='personalized_offers'),
-    # path('liked_jobs/', views.liked_jobs, name='liked_jobs'),
-    # path('recomended_jobs/', views.recomended_jobs, name='recomended_jobs'),
-    # path('login/', views.login, name='login'),
-    # path('register/', views.register, name='register'),
 ]
