0b65c06dc8b66107bfe7aab11ed2477a04df9cf3
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-21 19:55:51 +0200

    heroku_settings

--- job_finder_django/job_finder/urls.py
+++ job_finder_django/job_finder/urls.py
@@ -26,13 +26,13 @@ urlpatterns = [
     path('', include('user.urls')),
 
     # sumbit email for reset password
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     # send email
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_send_confirmation.html'), name='password_reset_done'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # send email with reset instructions
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='set_new_password.html'), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # send message that password was successfuly reseted
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
