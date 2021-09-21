c448283b805208e2efdc2979999bab4a08b71e45
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-21 19:42:34 +0200

    small_bugs

--- job_finder_django/jobs/forms.py
+++ job_finder_django/jobs/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class jobStatsForm(forms.Form): #Note that it is not inheriting from forms.ModelForm
     category = forms.CharField(widget=forms.TextInput(attrs={'class':'chosen-value', 'placeholder':'Search Category', 'autocomplete':'off'}))
-    job_title = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'Search Job Title (optional)', 'autocomplete':'off'}))
+    job_title = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'Search Job Title / Keyword (optional)', 'autocomplete':'off'}))
     location = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'Location (optional)', 'autocomplete':'off'}))
 
     junior = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'style':"all: revert", 'type':"checkbox"}))
